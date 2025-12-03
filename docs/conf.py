d051072283c4e9c6b98ac91a5daa9d407ab2cd36
wuqi <wuqi198772@gmail.com>
2025-12-03 15:07:28 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,9 @@ author = 'Wu Qi'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+	'sphinxcontrib.mermaid'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
